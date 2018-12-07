dd75a2e78fed13dec8aa66fdb6d8a93c80216e3b
Rinigus <rinigus.git@gmail.com>
2018-12-07 22:49:35 +0200

    add keys for flathub distribution

---
+++ apikeys.py
@@ -0,0 +1,30 @@
+# These keys are for Pure Maps distributed at Flathub only.
+#
+# Please provide your own API keys if you wish to distribute
+# Pure Maps somewhere else.
+# 
+# Please note that the included keys may stop functioning if
+# the allocated service limits are exceeded. In particular, that
+# can happen to Mapbox key.
+
+Keys = {
+    # https://developer.foursquare.com
+    "<FOURSQUARE_CLIENT>": "",
+    "<FOURSQUARE_SECRET>": "",
+
+    # mapbox.com
+    "<MAPBOX_KEY>": "pk.eyJ1IjoicmluaWd1cyIsImEiOiJjanBib3IyN3QweXF0M2tzYm5pbXIwejdpIn0.d63Ltm5lDFs2wT2lp_P-sg",
+    
+    # http://open.mapquestapi.com
+    "<MAPQUEST_KEY>": "W7Er5VxfGm7uMlAy40NIE2aU5olBlH0A",
+
+    # https://geocoder.opencagedata.com/api [old key]
+    "<OPENCAGE_KEY>": "0039dc979e2c48a09149d49c24937074",
+
+    # https://docs.stadiamaps.com/
+    "<STADIAMAPS_KEY>": "68e54742-9b15-4ca0-bc9c-ef9ab7aaea19",
+
+    # here.com
+    "<HERE_APP_ID>": "xt1AJYwdLQvzCZzKblGY",
+    "<HERE_APP_CODE>": "JZ8ShcgKmK_sJtR4CQauhA",
+}
